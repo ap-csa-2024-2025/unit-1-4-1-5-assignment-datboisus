26e66aba71233b17b76e7bb9e324c11d364044ca
datboisus <donvu251106@gmail.com>
2024-09-10 10:16:22 -0700

    0-1

--- Problem0.java
+++ Problem0.java
@@ -3,5 +3,10 @@ public class Problem0
   public static void main(String[] args)
   {
     // insert code here
+    double var1 = 2;
+    int var2 = (int)2.0;
+    
+    System.out.println(var1);
+    System.out.println(var2);
   }
 }
