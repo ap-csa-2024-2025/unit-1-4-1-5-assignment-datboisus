081b3f2c21025437350b303e4d2d35f5e4fbda07
datboisus <donvu251106@gmail.com>
2024-09-11 10:18:36 -0700

    change 2 to 3

--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,18 @@
+public class Problem3
+{
+    public static void main (String[] args)
+    {
+        int value = 1234;
+        System.out.println("Four digit number: " + value);
+        System.out.println("Here are the digits:");
+        System.out.println(value % 10);
+        value /=10;
+        System.out.println(value % 10);
+        value /=10;
+        System.out.println(value % 10);
+        value /=10;
+        System.out.println(value % 10);
+
+    }
+}
 
