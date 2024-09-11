d3d7f616df89b9ae61e0ab810e7d8030906e4663
datboisus <donvu251106@gmail.com>
2024-09-11 10:01:40 -0700

    1

---
+++ Problem1.java
@@ -0,0 +1,25 @@
+public class Problem1
+{
+    public static void main(String[] args)
+    {
+        int value = 24;
+        System.out.println("Value: " + value);
+        value++;
+        System.out.println("Value is now " + value);
+        value++;
+        System.out.println("Value is now " + value);
+        value++;
+        System.out.println("Value is now " + value);
+        value++;
+        System.out.println("Value is now " + value);
+        value--;
+        System.out.println("Value is now " + value);
+        value--;
+        System.out.println("Value is now " + value);
+        value--;
+        System.out.println("Value is now " + value);
+        value--;
+        System.out.println("Value is now " + value);
+    }
+}
+
