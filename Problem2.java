3a40e8ec0d7a27d058c32a744243270c23efd38c
datboisus <donvu251106@gmail.com>
2024-09-11 10:14:35 -0700

    2

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,15 @@
+public class Problem2
+{
+    public static void main (String[] args)
+    {
+        int value = 123;
+        System.out.println("Three digit number: " + value);
+        System.out.println("Here are the digits:");
+        System.out.println(value % 10);
+        value /=10;
+        System.out.println(value % 10);
+        value /=10;
+        System.out.println(value % 10);
 
+    }
+}
