b0a7fb81dae4ae401f49b62c8d2590102297038d
datboisus <donvu251106@gmail.com>
2024-09-12 09:44:45 -0700

    2

--- Problem2.java
+++ Problem2.java
@@ -1,7 +1,18 @@
-public class Problem3
+public class Problem2
 {
     public static void main (String[] args)
     {
-        
+        int value = 123;
+
+        System.out.println("Three Number: " + 123);
+
+        int hundred = value / 100;
+        int ten = (value / 10) % 10;
+        int one = value % 10;
+
+        System.out.println("Here are the digit:");
+        System.out.println(hundred);
+        System.out.println(ten);
+        System.out.println(one);
     }
 }
